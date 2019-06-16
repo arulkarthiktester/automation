e007b2b422a82991ffe71c308e0d9a0964b3a065
user <karthikanthan@gmail.com>
2019-06-16 12:39:18 +0530

    first commit

---
+++ src/test/java/myproject/one.java
@@ -0,0 +1,11 @@
+package myproject;
+
+import org.testng.annotations.Test;
+
+public class one {
+	@Test
+	  public void van()
+	  {
+	System.out.println("one done");	  
+	  }
+}
---
+++ src/test/java/myproject/two.java
@@ -0,0 +1,12 @@
+package myproject;
+
+import org.testng.annotations.Test;
+
+public class two
+{
+  @Test
+  public void car()
+  {
+System.out.println("two done");	  
+  }
+}
