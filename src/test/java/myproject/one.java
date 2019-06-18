6eea68763f89d8a11c5541aa80da0d435b4fda84
user <karthikanthan@gmail.com>
2019-06-18 22:35:39 +0530

    logging added

--- src/test/java/myproject/one.java
+++ src/test/java/myproject/one.java
@@ -1,20 +1,24 @@
 package myproject;
 
 import org.testng.annotations.Test;
-
+import org.apache.logging.log4j.*;
 public class one {
+	
+	private static Logger log =LogManager.getLogger(one.class.getName());
+	
 	@Test
 	public void van() {
-		System.out.println("one done");
+		
+		log.debug("debug message");
 	}
 
 	@Test
 	public void Auto() {
-		System.out.println("one done");
+		System.out.println("two done");
 	}
 
 	@Test
 	public void rock() {
-		System.out.println("one done");
+		System.out.println("three done");
 	}
 }
---
+++ src/test/java/myproject/three.java
@@ -0,0 +1,16 @@
+package myproject;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class three {
+	private static Logger log =LogManager.getLogger(three.class.getName());
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		log.debug("debug message");
+	}
+
+}
