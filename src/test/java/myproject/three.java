a945c87da89ffd9840b99f832f2c7019cba20aca
user <karthikanthan@gmail.com>
2019-06-23 11:20:27 +0530

    logs finished

--- src/test/java/myproject/three.java
+++ src/test/java/myproject/three.java
@@ -2,15 +2,19 @@ package myproject;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.testng.annotations.Test;
+import org.apache.logging.log4j.*;;
 
 public class three {
-	private static Logger log =LogManager.getLogger(three.class.getName());
+	
+	@Test
+	public void logs()
+	{
+Logger log =LogManager.getLogger(three.class.getName());
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		
 		log.debug("debug message");
+	
 	}
+	
 
 }
